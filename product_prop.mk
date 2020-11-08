cff356875a5ffc986dc1a62e482155a7b1f0924a
Jeong Seong Pil <jjjhitel@gmail.com>
2020-11-09 04:58:51 +0900

    picasso: Override some props

--- product_prop.mk
+++ product_prop.mk
@@ -19,6 +19,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera
 
+# Data Modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=false
+
+# RCS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rcs.supported=1
+
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.audio.fluence.voicerec=true \
@@ -26,8 +35,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
+    ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=33,22 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.NO_STAPA=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.volte.dan_support=true \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.radio.enable_temp_dds=true \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.redir_party_num=1 \
+    telephony.lteOnCdmaDevice=1 \
     persist.sys.fflag.override.settings_network_and_internet_v2=true
--- system_prop.mk
+++ system_prop.mk
@@ -18,3 +18,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.twsplussho=true \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
+
+# Blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
