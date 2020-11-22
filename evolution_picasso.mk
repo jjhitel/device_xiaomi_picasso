cdf4270c7f2a905a13c2233e34c7a06b6a594231
Joey Huab <joey@evolution-x.org>
2020-11-22 20:39:02 +0900

    raphael: Cleanup no longer used exports

--- evolution_picasso.mk
+++ evolution_picasso.mk
@@ -9,10 +9,6 @@ $(call inherit-product, device/xiaomi/picasso/device.mk)
 # Inherit some common Lineage stuff.
 EVO_BUILD_TYPE := UNOFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 WITH_GAPPS := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
